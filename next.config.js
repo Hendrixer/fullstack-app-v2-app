a4cae40a8c6044dfa6b41c98310ca35a78328e6b
Scott Moss <willscottmoss@gmail.com>
2022-12-21 14:50:20 -0600

    ts

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     appDir: true,
     serverComponentsExternalPackages: ['bcrypt'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
