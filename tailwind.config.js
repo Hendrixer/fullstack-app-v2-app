9edcc8d7137563a09e90aa271c17a6f601d9f425
Dustin Tauer <dustin@dtauer.com>
2023-02-27 20:44:00 -0600

    Update tailwind.config.js
    fixes #4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
-    './app/**/*.{tsx,jsx,js,ts}',
-    './page/**/*.{tsx,jsx,js,ts}',
-    './components/**/*.{tsx,jsx,js,ts}'
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-inter)"],
-      }
-    }
-  }
-}
+      },
+    },
+  },
+  plugins: [],
+}; 
